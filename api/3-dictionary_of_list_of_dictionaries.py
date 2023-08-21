99a56dd8ab7dfba3dec5266867f80c4582fee834
Axel Malka <axelmalka@outlook.fr>
2023-08-21 15:16:15 +0000

    fix format issues

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Python script that fetches and exports all tasks
-    from all employees in JSON format"""
+"""Python script that fetches and exports all tasks from all employees in JSON format"""
 
-import requests
 import json
+import requests
 
 
 def fetch_employee_data(employee_id):
-    to_do = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos\
-            ?userId={employee_id}', timeout=5)
-    names = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/\
-            {employee_id}', timeout=5)
+    to_do = requests.get(f'https://jsonplaceholder.typicode.com/\
+                         todos?userId={employee_id}', timeout=5)
+    names = requests.get(f'https://jsonplaceholder.typicode.com/\
+                         users/{employee_id}', timeout=5)
 
     if to_do.status_code == 200 and names.status_code == 200:
         json_todo = to_do.json()
@@ -28,10 +25,10 @@ def fetch_employee_data(employee_id):
                 for task in json_todo
             ]
         }
+
         return task_data
 
     return None
-
 
 if __name__ == "__main__":
     all_employee_data = {}
@@ -39,8 +36,10 @@ if __name__ == "__main__":
     for employee_id in range(1, 11):
         employee_data = fetch_employee_data(employee_id)
         if employee_data:
-            all_employee_data[str(employee_id)] = employee_data
+            all_employee_data[str(employee_id)] = [employee_data]
 
     json_filename = "todo_all_employees.json"
     with open(json_filename, 'w') as json_file:
         json.dump(all_employee_data, json_file)
+
+    print(f"Data exported to {json_filename}")
