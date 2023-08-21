d3d41570e8d13280759f74325e48fbc500325eca
Axel Malka <axelmalka@outlook.fr>
2023-08-21 09:54:15 +0000

    try new way

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
+"""Python script that for a given employee ID returns all his todo list"""
 
-"""import request and sys module"""
 import requests
 import sys
 
-if len(sys.argv) > 1:
-    user_id = sys.argv[1]
 
-    url_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    name_request = requests.get(url_name)
-    todos_request = requests.get(url_todos)
+if __name__ == "__main__":
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1], timeout=5)
+    names = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         sys.argv[1], timeout=5)
 
-    if name_request.status_code == 200:
-        name_response = name_request.json()
-        name = name_response["name"]
+    json_todo = to_do.json()
+    json_names = names.json()
 
-    if todos_request.status_code == 200:
-        todos_response = todos_request.json()
-        completed = []
-        uncompleted = []
-        for task in todos_response:
-            if task['completed']:
-                completed.append(task)
-            else:
-                uncompleted.append(task)
-        todos_total = (len(completed) + len(uncompleted))
+    all_tasks = 0
+    tasks_completed = 0
 
-    output = "Employee {} is done with tasks({}/{}):".format(
-        name, len(completed), todos_total)
-    print(output)
-    for task_name in completed:
-        print(f"\t {task_name['title']}")
-else:
-    print("Please provide a user_id as a command-line argument.")
+    titles_completed = []
+    for task in json_todo:
+        all_tasks += 1
+        if task["completed"] is True:
+            tasks_completed += 1
+            titles_completed.append(task["title"])
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(json_names['name'], tasks_completed, all_tasks))
+
+    for title_task in titles_completed:
+        print('\t {}'.format(title_task))
