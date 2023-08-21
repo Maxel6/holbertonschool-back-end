403d0f80e95d2c49a9f26f368542b8b71474013a
Axel Malka <axelmalka@outlook.fr>
2023-08-21 13:12:00 +0000

    export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Python script that for a given employee ID returns all his todo list and exports it to JSON"""
+
+import requests
+import json
+import sys
+
+if __name__ == "__main__":
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1], timeout=5)
+    names = requests.get('https://jsonplaceholder.typicode.com/users/' + sys.argv[1], timeout=5)
+
+    json_todo = to_do.json()
+    json_names = names.json()
+
+    task_data = {
+        sys.argv[1]: [
+            {
+                "username": json_names['username'],
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            for task in json_todo
+        ]
+    }
+
+    json_filename = f"{sys.argv[1]}.json"
+    with open(json_filename, 'w') as json_file:
+        json.dump(task_data, json_file)
